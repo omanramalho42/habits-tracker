e2aa252c6e2d531585e28e8b3757cde698d06aff
oman <omanapple42@hotmail.com>
2026-01-22 21:42:34 -0300

    fix/comment wizard mood

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 
   return (
     <>
-      <MoodWizard />
+      {/* <MoodWizard /> */}
 
       <main className="min-h-screen bg-background">
         <div className="max-w-5xl mx-auto px-4 py-8">
