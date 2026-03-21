a8205f6d7b16856475e7a28e1dda66debd2b368b
oman <omanapple42@hotmail.com>
2026-03-21 19:29:55 -0300

    fix/adjusting size of routine card

--- app/page.tsx
+++ app/page.tsx
@@ -226,7 +226,7 @@ export default function Home() {
                       variants={container}
                       initial="hidden"
                       animate="show"
-                      className="grid grid-cols-1 p-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-stretch mt-5 max-h-160 overflow-y-auto scroll-container"
+                      className="grid grid-cols-1 p-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-stretch mt-5 max-h-165 overflow-y-auto scroll-container"
                     >
                       {routines.map((routine, index) => (
                         <motion.div
